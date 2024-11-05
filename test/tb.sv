5d4617b2de7f446682dc33a05a633b43a79eea53
Jaaneshwaran <48589066+Jaaneshwaran@users.noreply.github.com>
2024-11-05 14:55:43 +0530

    Update tb.sv
    Updated the top module to be tt_um_wrapper_8bit_sha_256

--- test/tb.sv
+++ test/tb.sv
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:tt_um_example
- wrapper_8bit_sha_256 user_project (
+ tt_um_wrapper_8bit_sha_256 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
